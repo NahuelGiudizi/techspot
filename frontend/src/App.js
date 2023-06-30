1d53de058ac8af4ef98bf18184545199f9e39233
NahuelGiudizi <nahuelgiudizi@hotmail.com>
2023-06-30 01:32:48 -0300

    Products - ReactRouter - Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
-    <div>Welcome To TechSpot</div>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet/>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
