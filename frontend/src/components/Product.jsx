440f9d69f462b7634146ef8b0ba377049fc15d66
NahuelGiudizi <nahuelgiudizi@hotmail.com>
2023-06-30 18:10:57 -0300

    Product Details Page

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,6 +1,6 @@
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import Rating from "./Rating.jsx";
+import Rating from "./Rating";
 
 const Products = ({ product }) => {
   return (
@@ -10,7 +10,7 @@ const Products = ({ product }) => {
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as="div">
+          <Card.Title as="div" className="product-title">
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
