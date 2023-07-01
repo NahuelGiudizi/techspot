284b7d84786d4247fbaaeb656de5e0fca5c08bac
NahuelGiudizi <nahuelgiudizi@hotmail.com>
2023-07-01 02:48:39 -0300

    Mongoose & modeling data

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config()
+dotenv.config();
+import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000;
 import products from "./data/products.js";
+
+connectDB(); //Connect to MongoDB
 const app = express();
+
 
 app.get("/", (req, res) => {
   res.send("API is running...");
