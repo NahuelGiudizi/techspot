d0461aee69b683420079bc154d544faa64da369c
NahuelGiudizi <nahuelgiudizi@hotmail.com>
2023-07-03 16:45:51 -0300

    Qty & Add To Cart Handler

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -25,7 +25,7 @@ const cartSlice = createSlice({
       }
       // Calculate items price
       state.itemsPrice = addDecimals(
-        state.cartItems.reduce((acc, item) => acc + item.price * items.qty, 0)
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
       );
       // Calculate shipping price (If order is over $100 then free, else $10 shipping)
       state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
@@ -37,7 +37,7 @@ const cartSlice = createSlice({
       state.totalPrice = (
         Number(state.itemsPrice) +
         Number(state.shippingPrice) +
-        Number(stete.taxPrice)
+        Number(state.taxPrice)
       ).toFixed(2);
 
       localStorage.setItem("cart", JSON.stringify(state));
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
 import cartSliceReducer from "./slices/cartSlice";
 const store = configureStore({
-  reducer: { [apiSlice.reducerPath]: apiSlice.reducer },
+
+  reducer:{
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    cart: cartSliceReducer 
+  },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
