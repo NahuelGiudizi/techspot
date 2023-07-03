d4b0d236f60c512c6d43a1e1264d688ff60f34ae
NahuelGiudizi <nahuelgiudizi@hotmail.com>
2023-07-03 01:22:32 -0300

    Cart Slice & Reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
+import cartSliceReducer from "./slices/cartSlice";
 const store = configureStore({
   reducer: { [apiSlice.reducerPath]: apiSlice.reducer },
   middleware: (getDefaultMiddleware) =>
