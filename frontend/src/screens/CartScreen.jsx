6f14868470ea16f4b259a0abe469d81172e37d16
NahuelGiudizi <nahuelgiudizi@hotmail.com>
2023-07-04 03:30:18 -0300

    Remove From Cart

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -11,7 +11,7 @@ import {
 } from "react-bootstrap";
 import { FaTrash } from "react-icons/fa";
 import Message from "../components/Message";
-import { addToCart } from "../slices/cartSlice";
+import { addToCart, removeFromCart } from "../slices/cartSlice";
 
 const CartScreen = () => {
   const navigate = useNavigate();
@@ -22,7 +22,12 @@ const CartScreen = () => {
   const addToCartHandler = async (product, qty) => {
     dispatch(addToCart({ ...product, qty }));
   };
-
+  const removeFromCartHandler = async (id) => {
+    dispatch(removeFromCart(id));
+  };
+const checkoutHandler = ()=>{
+    navigate("/login?redirect=/shipping")
+}
   return (
     <Row>
       <Col md={8}>
@@ -64,7 +69,11 @@ const CartScreen = () => {
                     </Form.Control>
                   </Col>
                   <Col md={2}>
-                    <Button type="button" variant="light">
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => removeFromCartHandler(item._id)}
+                    >
                       <FaTrash></FaTrash>
                     </Button>
                   </Col>
@@ -92,6 +101,7 @@ const CartScreen = () => {
                 type="button"
                 className="btn-block"
                 disabled={cartItems.lenght === 0}
+                onClick={checkoutHandler}
               >
                 Proceed To Checkout
               </Button>
