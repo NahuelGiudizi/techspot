02c6d64d2cd49be3f5f4250ccdfe20de8cbac72a
NahuelGiudizi <nahuelgiudizi@hotmail.com>
2023-07-04 20:19:37 -0300

    User Email & Password Validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,19 @@ import User from "../models/userModel.js";
 //@route POST /api/users/login
 //@access Public
 const authUser = asyncHandler(async (req, res) => {
-  res.send("auth user");
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 //@desc Register user
@@ -61,18 +73,17 @@ const deleteUser = asyncHandler(async (req, res) => {
 //@route PUT /api/users/:id
 //@access Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
-    res.send("update user");
-  });
-
+  res.send("update user");
+});
 
 export {
-    authUser,
-    registerUser,
-    logoutUser,
-    getUserProfile,
-    updateUserProfile,
-    getUsers,
-    deleteUser,
-    getUserByID,
-    updateUser,
-}
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserByID,
+  updateUser,
+};
