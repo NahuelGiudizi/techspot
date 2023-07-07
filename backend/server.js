8fbd4bb442a2a77f82eb56ac70d3ab28e08e5266
NahuelGiudizi <nahuelgiudizi@hotmail.com>
2023-07-07 00:28:34 -0300

    Order Routes & Controller

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 const port = process.env.PORT || 5000;
 connectDB(); //Connect to MongoDB
@@ -19,13 +20,13 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
