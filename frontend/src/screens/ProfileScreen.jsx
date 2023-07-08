733519ac017edbce8986d907b9f59786a84dcc5e
NahuelGiudizi <nahuelgiudizi@hotmail.com>
2023-07-08 03:10:08 -0300

    Order Screen Bug Fixed

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -138,7 +138,7 @@ const ProfileScreen = () => {
                     )}
                   </td>
                   <td>
-                    <LinkContainer to={`/order/$order._ide`}>
+                    <LinkContainer to={`/order/${order._id}`}>
                       <Button className="btn-sm" variant="light">
                         Details
                       </Button>
