206c9ccd274c35dd65917e98bc5a3e946f7f3061
NahuelGiudizi <nahuelgiudizi@hotmail.com>
2023-07-09 14:44:31 -0300

    Admin Route Component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,16 +14,19 @@ import "./assets/styles/index.css";
 //import "./assets/styles/bootstrap.custom.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import PrivateRoute from "./components/PrivateRoute";
+import AdminRoute from "./components/AdminRoute";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen";
-import PrivateRoute from "./components/PrivateRoute";
+
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
+import OrderListScreen from "./screens/admin/OrderListScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 
 const router = createBrowserRouter(
@@ -41,6 +44,10 @@ const router = createBrowserRouter(
         <Route path="/placeorder" element={<PlaceOrderScreen />} />
         <Route path="/order/:id" element={<OrderScreen />} />
         <Route path="/profile" element={<ProfileScreen />} />
+      </Route>
+
+      <Route path="" element={<AdminRoute />}>
+        <Route path="/admin/orderlist" element={<OrderListScreen />} />
       </Route>
     </Route>
   )
