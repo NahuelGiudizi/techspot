4fa939bf913c72a82d35f197a2403ae271f588ba
NahuelGiudizi <nahuelgiudizi@hotmail.com>
2023-07-10 23:04:17 -0300

    List Users For Admins

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -7,7 +7,7 @@ const storage = multer.diskStorage({
   destination(req, file, cb) {
     cb(null, "uploads/");
   },
-  filename(req, res, cb) {
+  filename(req, file, cb) {
     cb(
       null,
       `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
