c7da2f2c704fc12d4827314bbc5446a1efba1ba3
NahuelGiudizi <nahuelgiudizi@hotmail.com>
2023-07-11 15:25:59 -0300

    Create Reviews Backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,20 +1,24 @@
-import express from "express";
+import express from 'express';
 const router = express.Router();
 import {
   getProducts,
-  getProductsById,
+  getProductById,
   createProduct,
   updateProduct,
   deleteProduct,
-} from "../controllers/productController.js";
+  createProductReview,
+  getTopProducts,
+} from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
+import checkObjectId from '../middleware/checkObjectId.js';
 
-import { protect, admin } from "../middleware/authMiddleware.js";
-
-router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.route('/:id/reviews').post(protect, checkObjectId, createProductReview);
+router.get('/top', getTopProducts);
 router
-  .route("/:id")
-  .get(getProductsById)
-  .put(protect, admin, updateProduct)
-  .delete(protect, admin, deleteProduct);
+  .route('/:id')
+  .get(checkObjectId, getProductById)
+  .put(protect, admin, checkObjectId, updateProduct)
+  .delete(protect, admin, checkObjectId, deleteProduct);
 
 export default router;
